c3b87f11279d7793e1f9cd51c469d84d85134442
y3v <yevgeni.kantorovich@gmail.com>
2018-06-07 18:36:45 -0400

    d

--- Agora/src/main/java/com/agora/controllers/DebateController.java
+++ Agora/src/main/java/com/agora/controllers/DebateController.java
@@ -22,43 +22,43 @@ public class DebateController {
 	@Autowired
 	private DebateService debateService;
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/getDebates")
 	public List<Debate> getDebates(){
 		return debateService.getAllDebates();
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping(method=RequestMethod.POST, value="/addDebate")
 	public List<Debate> addDebate(@RequestBody Debate debate){
 		return debateService.addDebate(debate);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/addDebator1/{userId}/{debateId}")
 	public void addDebator1(@PathVariable Long userId, @PathVariable Long debateId){
 		debateService.addDebator1(userId,debateId);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com" })
 	@RequestMapping("/addDebator2/{userId}/{debateId}")
 	public void addDebator2(@PathVariable Long userId, @PathVariable Long debateId){
 		debateService.addDebator2(userId,debateId);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/editDescription/{description}/{debateId}")
 	public void editDescription(@PathVariable String description, @PathVariable Long debateId){
 		debateService.editDescription(description, debateId);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/editTopic/{topic}/{debateId}")
 	public void editTopic(@PathVariable String topic, @PathVariable Long debateId){
 		debateService.editTopic(topic, debateId);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/deleteDebate/{debateId}")
 	public List<Debate> deleteDebate(@PathVariable Long debateId){
 		return debateService.deleteDebate(debateId);
--- Agora/src/main/java/com/agora/controllers/UserController.java
+++ Agora/src/main/java/com/agora/controllers/UserController.java
@@ -24,97 +24,97 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/getUsers")
 	public List<User> getUsers(){
 		return userService.getAllUsers();
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/getNotFollowing/{id}")
 	public List<User> getFollowing(@PathVariable Long id){
 		return userService.getAllNotFollowing(id);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping(method=RequestMethod.POST, value="/register")
 	public String registerUser(@RequestBody User user){
 		return userService.addUser(user);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping(method=RequestMethod.POST, value="/login")
 	public User login(@RequestBody LoginHolder loginHolder) {
 		return userService.getUser(loginHolder);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/addFriendship/{idReq}/{idFriend}")
 	public void addFriendship(@PathVariable Long idReq, @PathVariable Long idFriend){
 		userService.addFriendship(idReq,idFriend);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/removeFriendship/{idReq}/{idFriend}")
 	public void removeFriendship(@PathVariable Long idReq, @PathVariable Long idFriend){
 		userService.removeFriendship(idReq,idFriend);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/getFriends/{id}")
 	public List<User> getFriends(@PathVariable Long id){
 		return userService.getAllFriends(id);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/addUserLocation")
 	public void addUserLocation(@RequestBody UserLocation userLocation) {
 		userService.addUserLocation(userLocation);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/getLastUserLocation/{userId}/{count}")
 	public Set<UserLocation> getLastUserLocation(@PathVariable Long userId, @PathVariable int count){
 		return userService.getLastUserLocation(userId, count);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/getFriendsLocation/{userId}")
 	public List<UserLocation> getFriendsLocation(@PathVariable Long userId){
 		return userService.getFriendsLocation(userId);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/getFriendLocation/{userId}")
 	public UserLocation getFriendLocation(@PathVariable Long userId){
 		return userService.getFriendLocation(userId);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/deleteAllUserLocation/{userId}")
 	public void deleteAllUserLocation(@PathVariable long userId) {
 		userService.deleteAllUserLocation(userId);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping(method=RequestMethod.POST, value="/addProfilePic")
 	public void addProfilePic(@RequestBody Avatar photo) {
 		userService.addProfilePic(photo);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/getPhoto/{id}")
 	public String getPhoto(@PathVariable Long id){
 		return userService.getPhoto(id);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping("/getUser/{id}")
 	public User getUser(@PathVariable Long id){
 		return userService.getUser(id);
 	}
 	
-	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com"})
+	@CrossOrigin(origins = {"http://localhost:3000", "https://agora-admin.herokuapp.com" , "https://agoraproject.herokuapp.com"})
 	@RequestMapping(method=RequestMethod.POST, value="/edit")
 	public void editUser(@RequestBody User user){
 		userService.editUser(user);
